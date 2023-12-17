54f0a2cd6f71f3fe5f17846c063ae36b36147ec9
shreyascholke24 <99800418+shreyascholke24@users.noreply.github.com>
2023-12-17 19:57:15 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            
+            SANAM | About 
+        </title>
+        <link rel="stylesheet" href="sum.css">
+    </head>
+
+    <body class="sub-body">
+    
+        <header class="main-header">
+            <nav class="nav main-nav">
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+
+                </ul>
+            </nav>
+            <h1 class="band-name band-name-header">The Sanam</h1>
+        </header>
+        <section class="content container">
+            <h2 class="section-header">ABOUT</h2>
+            <img class="about-band-image" src="Images/sanam.jpg">
+            <p>Sanam is an Indian pop rock band formed in 2010 currently based in Mumbai, India known for its renditions of old classic Indian Bollywood songs as well as original music. The band SANAM consists of Sanam Puri (lead vocalist/composer), Samar Puri (lyricist/lead guitar/composer), Venky S or Venkat Subramaniyam (bass guitar) and Keshav Dhanraj.In 2016, the band is amongst India's top 10 independent YouTube Channels, India's biggest music artist on the digital platform and the fastest growing YouTube channel in the country.</p>             
+            <p>Winners of Times Music Supastars in 2010 as SQS Project.
+                Awarded for Best Music Content Creator- (National Category) at the Social Media Summit & Awards 2018
+                Awarded "Digital Icons of the year" 2018 by Talent Track Awards.
+                Awarded Dadasaheb Phalke International Film Festival Award for the Live Band 2019 
+                Awarded Radio City Freedom Award 2019 for Best Pop song of the year for "Itni Door".</p><br>
+        </section>
+        <footer class="main-footer">
+            <h3 class="band-name footer-band-name">The Sanam</h3>
+            <ul>
+                <li>
+                    <a href="https://spotify.com">
+                    <img src="Images/spotify.png">
+                    </a>
+                </li>
+
+                <li>
+                    <a href="https://www.youtube.com/c/sanamk.022">
+                    <img src="Images/youtube.png"  >
+                    </a>
+                </li>
+                <li>
+                    <a href="https://facebook.com">
+                    <img src="Images/facebook.png" >
+                    </a>
+                </li>
+                <li>
+                    <a href="https://instagram.com">
+                    <img src="Images/instagram2.png" >
+                    </a>
+                </li>
+            </ul>
+        </footer>
+
+    </body>
+</html>
